7db4f00568e16633cfe67e9714f3774348786d54
Alice Campos <alicecarolinacc271@gmail.com>
2026-03-03 08:36:37 -0300

    03.03

---
+++ senha.py
@@ -0,0 +1,15 @@
+senha_correta = 123456
+senha = int(input("Digite a senha: "))
+nome = (input("Digite seu nome:"))
+
+if senha == senha_correta:
+    print("Olá, SEUNOME. Seja bem-vindo ao nosso banco!")
+else:
+    print("Senha incorreta! Você ainda tem 2 tentativas.")
+    
+    senha = int(input("Digite a senha: "))
+    
+    if senha == senha_correta:
+        print("Olá. Seja bem-vindo ao nosso banco!")
+    else:
+        print("Senha incorreta! Você ainda tem 1 tentativa.")

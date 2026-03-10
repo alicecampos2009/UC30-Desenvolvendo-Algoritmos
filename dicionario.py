c6b0249919861aaa66972c3c24df81bbae7944c9
Alice Campos <alicecarolinacc271@gmail.com>
2026-03-10 08:40:32 -0300

    Aula 10.03

---
+++ dicionario.py
@@ -0,0 +1,39 @@
+# Sem dicionário 
+matricula1 = 2026001
+nome1 = "Ana Silva"
+telefone1 = "9999-8888"
+
+# Com dicionário
+aluno = {
+    "matricula": 2026001,
+    "nome": "Ana Silva",
+    "telefone": "9999-8888"
+}
+
+print(aluno) 
+
+contato = {
+    "@camila": "Camila",
+    "@paola": "Paola",
+    "@sheron": "Sheron",
+    "@bruna": "Bruna"
+}
+
+print(contato)
+print(type(contato)) 
+
+# Dicionário vazio
+vazio = {}
+
+# Dicionário com tipos mistos
+dados = {
+    "nome": "João",
+    "idade": 25,
+    "altura": 1.70,
+    "ativo": True
+}
+
+print
+
+("Vazio: ", vazio)
+print("Vazio: ", dados)   

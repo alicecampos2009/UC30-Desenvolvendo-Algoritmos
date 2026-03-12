052719de23cda445eb0c5373d9a663d98fc6bd50
Alice Campos <alicecarolinacc271@gmail.com>
2026-03-12 08:37:02 -0300

    Add files via upload

---
+++ atividadeFunçaoSoma.py
@@ -0,0 +1,13 @@
+def calculoSoma(valor1, valor2):
+    soma = (valor1 + valor2)
+    return soma
+
+resultado = calculoSoma (5.0, 7.0)
+print(f"O resultado da soma é de: {resultado}")
+
+def calculoMulti(valor1, valor2):
+    multi = (valor1 * valor2)
+    return multi
+
+resultado = calculoMulti (2, 4)
+print(f"O resultado da multiplicação é de: {resultado}")
